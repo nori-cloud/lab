555a540886024207e59bf042aa7934668745f677
norriswu0 <norris.wu.au+github@outlook.com>
2025-08-17 03:46:46 +0000

    add ecr lifecycle policy

--- cloud/scheduler-trigger/main.tf
+++ cloud/scheduler-trigger/main.tf
@@ -31,6 +31,40 @@ resource "aws_ecr_repository" "this" {
   image_tag_mutability = "MUTABLE"
 }
 
+resource "aws_ecr_lifecycle_policy" "this" {
+  repository = aws_ecr_repository.this.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Delete untagged images after 3 days"
+        selection = {
+          tagStatus   = "untagged"
+          countType   = "sinceImagePushed"
+          countUnit   = "days"
+          countNumber = 3
+        }
+        action = {
+          type = "expire"
+        }
+      },
+      {
+        rulePriority = 2
+        description  = "Keep only the 10 most recent tagged images"
+        selection = {
+          tagStatus     = "tagged"
+          countType     = "imageCountMoreThan"
+          countNumber   = 10
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
+
 # NEED TO APPLY THE ABOVE FIRST
 
 data "aws_iam_policy_document" "assume_role" {
