f64976727458a8424d2ad09355d678bde721affc
norriswu0 <norris.wu.au+github@outlook.com>
2025-08-17 06:27:01 +0000

    add role to auth scheduler trigger github action

---
+++ cloud/scheduler-trigger/infra/ga-role.tf
@@ -0,0 +1,201 @@
+variable "allowed_repos" {
+  type        = list(string)
+  description = "List of allowed repositories"
+  default     = ["nori-cloud/infra"]
+}
+
+data "aws_iam_policy_document" "github_actions_assume_role" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Federated"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/token.actions.githubusercontent.com"]
+    }
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = [for repo in var.allowed_repos : "repo:${repo}:*"]
+    }
+  }
+}
+
+resource "aws_iam_role" "scheduler_trigger_ga_role" {
+  name               = "infra-scheduler-trigger-ga-role"
+  assume_role_policy = data.aws_iam_policy_document.github_actions_assume_role.json
+}
+
+data "aws_iam_policy_document" "github_actions_permissions" {
+  # ECR permissions for building and pushing images
+  statement {
+    effect = "Allow"
+    actions = [
+      "ecr:GetAuthorizationToken",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+      "ecr:BatchImportLayer",
+      "ecr:PutImage",
+      "ecr:InitiateLayerUpload",
+      "ecr:UploadLayerPart",
+      "ecr:CompleteLayerUpload",
+      "ecr:DescribeRepositories",
+      "ecr:DescribeImages",
+      "ecr:CreateRepository",
+      "ecr:PutLifecyclePolicy"
+    ]
+    resources = ["*"]
+  }
+
+  # Lambda permissions
+  statement {
+    effect = "Allow"
+    actions = [
+      "lambda:CreateFunction",
+      "lambda:UpdateFunctionCode",
+      "lambda:UpdateFunctionConfiguration",
+      "lambda:GetFunction",
+      "lambda:DeleteFunction",
+      "lambda:AddPermission",
+      "lambda:RemovePermission",
+      "lambda:ListFunctions",
+      "lambda:TagResource",
+      "lambda:UntagResource"
+    ]
+    resources = ["*"]
+  }
+
+  # IAM permissions for creating roles and policies
+  statement {
+    effect = "Allow"
+    actions = [
+      "iam:CreateRole",
+      "iam:DeleteRole",
+      "iam:GetRole",
+      "iam:ListRoles",
+      "iam:UpdateRole",
+      "iam:AttachRolePolicy",
+      "iam:DetachRolePolicy",
+      "iam:ListAttachedRolePolicies",
+      "iam:CreatePolicy",
+      "iam:DeletePolicy",
+      "iam:GetPolicy",
+      "iam:GetPolicyVersion",
+      "iam:ListPolicies",
+      "iam:ListPolicyVersions",
+      "iam:TagRole",
+      "iam:UntagRole",
+      "iam:TagPolicy",
+      "iam:UntagPolicy"
+    ]
+    resources = ["*"]
+  }
+
+  # SQS permissions for DLQ
+  statement {
+    effect = "Allow"
+    actions = [
+      "sqs:CreateQueue",
+      "sqs:DeleteQueue",
+      "sqs:GetQueueAttributes",
+      "sqs:SetQueueAttributes",
+      "sqs:TagQueue",
+      "sqs:UntagQueue",
+      "sqs:ListQueues"
+    ]
+    resources = ["*"]
+  }
+
+  # EventBridge Scheduler permissions
+  statement {
+    effect = "Allow"
+    actions = [
+      "scheduler:CreateSchedule",
+      "scheduler:DeleteSchedule",
+      "scheduler:GetSchedule",
+      "scheduler:UpdateSchedule",
+      "scheduler:ListSchedules",
+      "scheduler:CreateScheduleGroup",
+      "scheduler:DeleteScheduleGroup",
+      "scheduler:GetScheduleGroup",
+      "scheduler:ListScheduleGroups",
+      "scheduler:TagResource",
+      "scheduler:UntagResource"
+    ]
+    resources = ["*"]
+  }
+
+  # CloudWatch permissions for logs and alarms
+  statement {
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:DeleteLogGroup",
+      "logs:DescribeLogGroups",
+      "logs:PutRetentionPolicy",
+      "logs:TagLogGroup",
+      "logs:UntagLogGroup",
+      "cloudwatch:PutMetricAlarm",
+      "cloudwatch:DeleteAlarms",
+      "cloudwatch:DescribeAlarms",
+      "cloudwatch:TagResource",
+      "cloudwatch:UntagResource"
+    ]
+    resources = ["*"]
+  }
+
+  # S3 permissions for Terraform state
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:DeleteObject",
+      "s3:ListBucket"
+    ]
+    resources = [
+      "arn:aws:s3:::infra-tf-033b4055b800d083",
+      "arn:aws:s3:::infra-tf-033b4055b800d083/*"
+    ]
+  }
+
+  # DynamoDB permissions for Terraform state locking (if using)
+  statement {
+    effect = "Allow"
+    actions = [
+      "dynamodb:GetItem",
+      "dynamodb:PutItem",
+      "dynamodb:DeleteItem"
+    ]
+    resources = ["arn:aws:dynamodb:*:*:table/terraform-locks"]
+  }
+
+  # General permissions for resource discovery
+  statement {
+    effect = "Allow"
+    actions = [
+      "sts:GetCallerIdentity",
+      "account:GetAlternateContact",
+      "account:GetContactInformation"
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "github_actions_policy" {
+  name        = "infra-scheduler-trigger-ga-policy"
+  description = "Policy for GitHub Actions to deploy scheduler trigger infrastructure"
+  policy      = data.aws_iam_policy_document.github_actions_permissions.json
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions_policy_attachment" {
+  role       = aws_iam_role.scheduler_trigger_ga_role.name
+  policy_arn = aws_iam_policy.github_actions_policy.arn
+}
+
+data "aws_caller_identity" "current" {}
