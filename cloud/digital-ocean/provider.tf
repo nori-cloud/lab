bf5f831e598188f22d02d450517a585822f57d19
norriswu0 <norris.wu.au+github@outlook.com>
2025-09-05 09:57:02 +0000

    fix helm provider configuration

--- cloud/digital-ocean/provider.tf
+++ cloud/digital-ocean/provider.tf
@@ -17,15 +17,9 @@ provider "kubernetes" {
 provider "helm" {
   kubernetes = {
     host = module.cluster.host
+    token = module.cluster.token
     cluster_ca_certificate = base64decode(
       module.cluster.ca_certificate
     )
-
-    exec = {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      command     = "doctl"
-      args = ["kubernetes", "cluster", "kubeconfig", "exec-credential",
-      "--version=v1beta1", module.cluster.id]
-    }
   }
 }
