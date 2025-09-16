a710a77ba02c2d6529ca5d1595b45c84918cba9d
norriswu0 <norris.wu.au+github@outlook.com>
2025-09-16 14:41:21 +0000

    Revert "disable proxmox module"
    This reverts commit 10ff64ee12bc91712eb17596044ea807ffcd77c2.

--- home/main.tf
+++ home/main.tf
@@ -1,10 +1,10 @@
 terraform {
   required_providers {
     # https://registry.terraform.io/providers/bpg/proxmox/latest/docs
-    # proxmox = {
-    #   source  = "bpg/proxmox"
-    #   version = "0.83.2"
-    # }
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "0.83.2"
+    }
     # https://registry.terraform.io/providers/hashicorp/kubernetes/latest/docs
     kubernetes = {
       source  = "hashicorp/kubernetes"
@@ -34,19 +34,15 @@ variable "proxmox_api_token" {
   sensitive = true
 }
 
-# provider "proxmox" {
-#   endpoint  = "https://192.168.0.100:8006/"
-#   api_token = var.proxmox_api_token
-#   insecure  = true
-#   ssh {
-#     agent    = true
-#     username = var.proxmox_username
-#   }
-# }
-
-# module "proxmox" {
-#   source = "./proxmox"
-# }
+provider "proxmox" {
+  endpoint  = "https://192.168.0.100:8006/"
+  api_token = var.proxmox_api_token
+  insecure  = true
+  ssh {
+    agent    = true
+    username = var.proxmox_username
+  }
+}
 
 provider "kubernetes" {
   config_path = "~/.kube/config"
@@ -60,6 +56,10 @@ provider "helm" {
 
 locals {
   name = "nori-cloud"
+}
+
+module "proxmox" {
+  source = "./proxmox"
 }
 
 module "cluster" {
