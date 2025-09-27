4e52210b5bb6a85dbad57271cb51c9370bb23ec2
norriswu0 <norris.wu.au+github@outlook.com>
2025-09-27 13:33:25 +0000

    add teleport cluster

---
+++ home/system/teleport.tf
@@ -0,0 +1,37 @@
+resource "kubernetes_manifest" "teleport-cluster-certificate-secret" {
+  manifest = {
+    apiVersion = "cert-manager.io/v1"
+    kind = "Certificate"
+    metadata = {
+      name = "teleport-certificate"
+      namespace = kubernetes_namespace.this.metadata[0].name
+    }
+    spec = {
+      secretName = "teleport-certificate-secret"
+      issuerRef = {
+        name = "cloudflare-cluster-issuer-production"
+        kind = "ClusterIssuer"
+      }
+      commonName = "teleport.norriswu.me"
+      dnsNames = [
+        "teleport.norriswu.me",
+        "*.teleport.norriswu.me"
+      ]
+    }
+  }
+}
+
+resource "helm_release" "teleport" {
+  depends_on = [
+    kubernetes_manifest.teleport-cluster-certificate-secret
+  ]
+
+  namespace = kubernetes_namespace.this.metadata[0].name
+
+  name = "teleport"
+  repository = "https://charts.releases.teleport.dev"
+  chart = "teleport-cluster"
+  version = "18.2.2"
+  
+  values = [file("${path.module}/teleport-cluster-values.yaml")]
+}
