632a75df8c486905d822c1a17f88877d2b223f0f
norriswu0 <norris.wu.au+github@outlook.com>
2025-10-05 04:43:44 +0000

    refactor teleport auth setup script to work with fresh install

--- home/script/teleport-auth-setup.sh
+++ home/script/teleport-auth-setup.sh
@@ -18,16 +18,19 @@ fi
 TEMP_CONNECTOR_FILE=/tmp/github-connector.yaml
 ROLE_FILE=./script/teleport-k3s-admin-role.yaml
 
+# if no access to cluster via tsh
+# kubectl exec -n system -i deployment/teleport-auth -- tctl
+
 echo "Creating role resource via definition in \"$ROLE_FILE\"..."
-cat $ROLE_FILE | tctl create -f
+cat $ROLE_FILE | kubectl exec -n system -i deployment/teleport-auth -- tctl create -f
 
 echo "Configuring github sso on teleport cluster..."
-tctl sso configure github \
-  --id $CLIENT_ID \
-  --secret $CLIENT_SECRET \
+kubectl exec -n system -i deployment/teleport-auth -- tctl sso configure github \
+  --id $TELEPORT_OAUTH_CLIENT_ID \
+  --secret $TELEPORT_OAUTH_CLIENT_SECRET \
   --teams-to-roles=nori-cloud,teleport-admins,access,editor,k3s-admin \
   > $TEMP_CONNECTOR_FILE
 
-cat $TEMP_CONNECTOR_FILE | tctl create -f
+cat $TEMP_CONNECTOR_FILE | kubectl exec -n system -i deployment/teleport-auth -- tctl create -f
 
 echo "Done configuring auth on teleport cluster."
