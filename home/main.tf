c2523d93938687a30ca12083897bde2d3264b240
norriswu0 <norris.wu.au+github@outlook.com>
2025-10-11 13:17:53 +0000

    manually remove proxmox related resource from state

--- home/main.tf
+++ home/main.tf
@@ -1,10 +1,10 @@
 terraform {
   required_providers {
     # https://registry.terraform.io/providers/bpg/proxmox/latest/docs
-    proxmox = {
-      source  = "bpg/proxmox"
-      version = "0.83.2"
-    }
+    # proxmox = {
+    #   source  = "bpg/proxmox"
+    #   version = "0.83.2"
+    # }
     # https://registry.terraform.io/providers/hashicorp/kubernetes/latest/docs
     kubernetes = {
       source  = "hashicorp/kubernetes"
@@ -34,15 +34,15 @@ variable "proxmox_api_token" {
   sensitive = true
 }
 
-provider "proxmox" {
-  endpoint  = "https://192.168.0.100:8006/"
-  api_token = var.proxmox_api_token
-  insecure  = true
-  ssh {
-    agent    = true
-    username = var.proxmox_username
-  }
-}
+# provider "proxmox" {
+#   endpoint  = "https://192.168.0.100:8006/"
+#   api_token = var.proxmox_api_token
+#   insecure  = true
+#   ssh {
+#     agent    = true
+#     username = var.proxmox_username
+#   }
+# }
 
 provider "kubernetes" {
   config_path = "~/.kube/config"
@@ -58,9 +58,9 @@ locals {
   name = "nori-cloud"
 }
 
-module "proxmox" {
-  source = "./proxmox"
-}
+# module "proxmox" {
+#   source = "./proxmox"
+# }
 
 module "cluster" {
   source = "./cluster"
