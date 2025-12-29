2c3ac6da8800330388e5e454b5f680e44d110c6a
norriswu0 <norris.wu.au+github@outlook.com>
2025-12-29 14:21:13 +0000

    add tailscale svc to connect to traefik

--- home/networking/traefik.tf
+++ home/networking/traefik.tf
@@ -13,3 +13,40 @@ resource "helm_release" "traefik" {
 
   values = [file("${path.module}/traefik-values.yaml")]
 }
+
+# Secondary LoadBalancer service for Tailscale access
+resource "kubernetes_service_v1" "traefik_tailscale" {
+  depends_on = [helm_release.traefik]
+
+  metadata {
+    name      = "traefik-tailscale"
+    namespace = kubernetes_namespace.this.metadata[0].name
+    annotations = {
+      "tailscale.com/hostname" = "traefik"
+    }
+  }
+
+  spec {
+    type                = "LoadBalancer"
+    load_balancer_class = "tailscale"
+
+    selector = {
+      "app.kubernetes.io/name"     = "traefik"
+      "app.kubernetes.io/instance" = "traefik"
+    }
+
+    port {
+      name        = "web"
+      port        = 80
+      target_port = "web"
+      protocol    = "TCP"
+    }
+
+    port {
+      name        = "websecure"
+      port        = 443
+      target_port = "websecure"
+      protocol    = "TCP"
+    }
+  }
+}
